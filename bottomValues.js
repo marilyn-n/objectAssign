bfbd0e70e361592f4ac094237de60547e3a443e5
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-09-29 19:09:42 -0500

    logical operators

--- bottomValues.js
+++ bottomValues.js
@@ -24,3 +24,36 @@ console.log(isMarried(obj))
 
 const allBottomValues = bottomValues.filter(item => !item)
 console.log(allBottomValues)
+
+
+// You have a friend who can't eat "fish" or "pineapples". 
+// Write a function using && to check if she can eat a particular kind of food or not. 
+// Return one sentence if she can eat it, and a different one if she can't. 
+// For example, the function might work like this: canEllaEat("panda stew");
+//  would return "She can eat it!".
+
+const canSheEat = (food) => {
+  if (food === 'panda stew') {
+    return ('She can eat it!')
+  } else if (food === 'pineaple' && 'fish') {
+    return ('She cannot eat it')
+  }
+}
+
+console.log(canSheEat('panda stew'))
+// Rewrite the previous function using || instead.
+
+const canSheEat2 = (food) => {
+  if (food === 'panda stew') {
+    return ('She can eat it!')
+  } else if (food === 'pineaple' || 'fish') {
+    return ('She cannot eat it')
+  }
+}
+
+console.log(canSheEat2(!'panda stew', '<<<<<<'))
+
+// Let's add a new message to the number guessing game we made in the previous lesson.
+//  Since the rules of the game say that the user has to guess a winning number which will 
+//  be between 1 and 20, make the program output "That's not between 1 and 20!" 
+//  if the user guesses less than 1 or greater than 20.
